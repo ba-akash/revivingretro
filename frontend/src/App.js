84b203d29c804bd8df39fb69bca775b8c3d7f171
Akash Manhas <akash.manhas.1111@gmail.com>
2024-03-21 21:01:14 -0400

    adding more basic structure for the working project

---
+++ frontend/src/App.js
@@ -0,0 +1,57 @@
+import "./App.css";
+import "./css/custom.css";
+import "./css/style.default.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { PersistGate } from "redux-persist/integration/react";
+import publicRoutes from "./Routes/routes";
+import DefaultLayout from "./Layouts/DefaultLayout";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <Routes>
+              {publicRoutes.map((route, index) => {
+                const Component = route.component;
+                return (
+                  <Route
+                    key={index}
+                    path={route.path}
+                    element={
+                      <DefaultLayout>
+                        <Component />
+                      </DefaultLayout>
+                    }
+                  ></Route>
+                );
+              })}
+              
+            </Routes>
+          </PersistGate>
+        </Provider>
+      </BrowserRouter>
+      <ToastContainer
+        position="bottom-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </div>
+  );
+}
+
+export default App;
